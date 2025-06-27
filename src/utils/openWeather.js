91c49caac7bbf0877ff807abddf86fbe295d6d8c
tjdux <tjduxxx@gmail.com>
2025-06-27 18:38:47 +0900

    수정

--- src/utils/openWeather.js
+++ src/utils/openWeather.js
@@ -1,7 +1,7 @@
 import OpenWeather from "openweathermap-ts";
 
 const openWeather = new OpenWeather({
-  apiKey: "3cf55ce2735be632614ff860741f8796"
+  apiKey: "",
 });
 
 openWeather.setUnits("metric");
